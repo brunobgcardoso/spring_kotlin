9d6255d1deb991f7d6f98270e85f40ce2d2679cd
Bruno Braga <brunobgcardoso@yahoo.com.br>
2021-03-23 20:40:48 -0300

    Adding Profiles

--- src/main/kotlin/com/braga/demo/controller/JSONController.kt
+++ src/main/kotlin/com/braga/demo/controller/JSONController.kt
@@ -2,12 +2,16 @@ package com.braga.demo.controller
 
 import com.braga.demo.model.Cliente
 import com.braga.demo.model.Telefone
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
 
 @RestController
-class JSONController {
+class JSONController(private val environment: Environment) {
+    @Value("\${spring.profiles.active}")
+    lateinit var profile : String
 
     @GetMapping("/cliente")
     fun getCliente(): Cliente {
